4f635daaa0f61e78bb91fd9ff02e433dcde7e19a
David Condrey <davidcondrey@me.com>
2026-02-13 12:44:24 -0800

    remove vendored dependencies

--- rust/witnessd-core/vendor/aws-lc-sys-0.37.0/aws-lc/include/openssl/boringssl_prefix_symbols.h
+++
@@ -1,5 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Commercial OR ISC
-
-// This file is intentionally empty, were AWS-LC installed with symbol prefixes, this file would contain
-// macro definitions for the prefixed symbols.
--- rust/witnessd-core/vendor/aws-lc-sys-0.37.0/builder/cc_builder/linux_ppc64le.rs
+++
@@ -1,9 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Commercial OR ISC
-// Thu Jan 22 16:28:54 UTC 2026
-
-pub(super) const CRYPTO_LIBRARY: &[&str] = &[
-    "generated-src/linux-ppc64le/crypto/fipsmodule/aesp8-ppc.S",
-    "generated-src/linux-ppc64le/crypto/fipsmodule/ghashp8-ppc.S",
-    "generated-src/linux-ppc64le/crypto/test/trampoline-ppc.S",
-];
